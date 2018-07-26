93483abea817973d813094e3676869ef4008ef34
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-07-26 21:00:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   elsif array.size > 2
-    # array.insert(-2, "and ")
-    # array.join(", ")
-    
+    array.insert(-2, "and ")
+    array.join(", ")
   end
 end
